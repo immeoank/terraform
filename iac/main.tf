3866ad597bc45532f1303b32233684a355c2ca2e
Anders Nannerup <ank@immeo.dk>
2023-11-10 15:40:58 +0100

    changes 6

--- iac/main.tf
+++ iac/main.tf
@@ -2,21 +2,20 @@ terraform {
   backend "azurerm" {
     # Backend config here
   }
-
-
-
-  module "frontend_rg" {
-    source   = "./modules/resource_group"
-    name     = "sascargo-${var.environment}-web-frontend-rg"
-    location = var.location
-  }
-
-  module "backend_rg" {
-    source   = "./modules/resource_group"
-    name     = "sascargo-${var.environment}-web-backend-rg"
-    location = var.location
-  }
 }
+
+module "frontend_rg" {
+  source   = "./modules/resource_group"
+  name     = "sascargo-${var.environment}-web-frontend-rg"
+  location = var.location
+}
+
+module "backend_rg" {
+  source   = "./modules/resource_group"
+  name     = "sascargo-${var.environment}-web-backend-rg"
+  location = var.location
+}
+
 provider "azurerm" {
   features {}
 }
